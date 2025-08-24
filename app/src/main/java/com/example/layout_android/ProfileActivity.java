2a8f995d9d6fc163aab6e88193e5aff4bbaf21d4
mrskenchan <kenjiro.mendez@gmail.com>
2025-08-24 16:35:38 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/layout_android/ProfileActivity.java
@@ -0,0 +1,11 @@
+package com.example.layout_android;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
