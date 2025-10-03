c4c1dac9af7660d1fdb8c4dd4e54e22c54e11f6a
Hankk21 <saitam.aollu432@gmail.com>
2025-10-02 23:30:32 -0300

    mini arreglo

--- app/src/main/java/com/example/layout_android/DBHelper.java
+++ app/src/main/java/com/example/layout_android/DBHelper.java
@@ -3,28 +3,31 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBHelper extends SQLiteOpenHelper { //DEFINICION DE LAS CLASES
-    public static final int DATABASE_VERSION = 1:
-    public static final String DATABASE_NAME = "":
+public class DBHelper extends SQLiteOpenHelper { // DEFINICIÓN DE LA CLASE
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FoodDB.db";
 
-    private  static final String SQL_CREATE_ENTRIES =  //SENTENCIAS SQL
-            "CREATE TABLE " + FoodContract,FoodEntry,TABLE_NAME + " (" +
-                    FoodContract.FoodEntry.COLUMN_ID + " INTEGER PRIMARY KEY" +
-                    FoodContract.FoodEntry.COLUMN_NAME + " TEXT," +
-                    FoodContract.FoodEntry.COLUMN_DESCRIPTION + "TEXT)" +:
+    // SENTENCIAS SQL
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FoodContract.FoodEntry.TABLE_NAME + " (" +
+                    FoodContract.FoodEntry.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    FoodContract.FoodEntry.COLUMN_NAME + " TEXT, " +
+                    FoodContract.FoodEntry.COLUMN_DESCRIPTION + " TEXT)";
 
-    public DBHelper(Context context) {  //CONSTRUCTOR
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FoodContract.FoodEntry.TABLE_NAME;
+
+    // CONSTRUCTOR
+    public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
-    @Override   //METODO ONCREATE
-    public void onCreate(SQLiteOpenHelper db) {
-        db.execSQL(SQL_CREATE_ENTRIES):
+    @Override   // MÉTODO ONCREATE
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
-
-    @Override  // METODO ONUPGRADE
-    public void onUpgrade(SQLiteOpenHelper db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + FoodContract.FoodEntry.TABLE_NAME):
+    @Override  // MÉTODO ONUPGRADE
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
         onCreate(db);
     }
 }
